9ce68e993876a6050036fd31acc8415e18957540
Vivekanand P v <vivekanandpv@outlook.com>
2024-01-08 22:02:44 +0530

    done

---
+++ src/main/java/com/vivekanandpv/springbootcircuitbreakerdownstream/SpringBootCircuitBreakerDownstreamApplication.java
@@ -0,0 +1,13 @@
+package com.vivekanandpv.springbootcircuitbreakerdownstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCircuitBreakerDownstreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCircuitBreakerDownstreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vivekanandpv/springbootcircuitbreakerdownstream/SpringBootCircuitBreakerDownstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivekanandpv.springbootcircuitbreakerdownstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCircuitBreakerDownstreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
